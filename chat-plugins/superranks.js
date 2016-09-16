6610ecbd6eaf572c819c16046846abc85a7beb85
CelestKimber <celestkimberly@gmail.com>
2016-09-16 15:17:27 -0700

    Update superranks.js

--- chat-plugins/superranks.js
+++ chat-plugins/superranks.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 global.SuperRanks = {
 	ranks: {},
 	isHoster: function (userid) {
-		if (userid === 'celestiaprincess') return true;
+		if (userid === 'presidentaaether') return true;
 		if (this.ranks[userid] && this.ranks[userid] === "h") return true;
 		return false;
 	},
